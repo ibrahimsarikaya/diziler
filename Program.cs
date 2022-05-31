40d9cace7a53c6b6505d41cd1f61596b58c41f42
ibrahimsarikaya <ibo7534@gmail.com>
2022-05-31 16:16:42 +0300

    Codding

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace diziler // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Dizi tanımlama
+            string[] renkler = new string[5];
+            string[] hayvanlar = {"Kedi","Köpek","Kuş","Maymun"};
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //Dizilere değer atama ve erişim
+            renkler[0]="Mavi";
+            dizi[3]= 10;
+            Console.WriteLine(hayvanlar[0]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+            
         }
     }
 }
