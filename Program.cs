6e19527b3643a1111a2950e3c619c302f134aac1
ibrahimsarikaya <ibo7534@gmail.com>
2022-05-31 16:07:33 +0300

    Codding

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace diziler // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
